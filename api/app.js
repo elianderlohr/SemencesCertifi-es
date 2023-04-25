147896f72c47285c020cfd9c10f5561fc0cd1f1e
elianderlohr <elias.anderlohr@t-online.de>
2023-04-25 13:36:41 +0200

    fix: update

--- api/app.js
+++ api/app.js
@@ -88,11 +88,11 @@ if (process.env.NODE_ENV === "production") {
   // CORS
 
   const cors = require("cors");
-  app.use(cors({ credentials: true, origin: "http://localhost:8080" }));
+  app.use(cors({ origin: "*" }));
 
   app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Methods",
       "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
