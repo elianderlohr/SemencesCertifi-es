592b96e8575dedecb03fc6ca3b8d6876b054e629
elianderlohr <elias.anderlohr@t-online.de>
2023-04-25 12:28:54 +0200

    fix: add index js

---
+++ frontend/js/index.js
@@ -0,0 +1,30 @@
+const _handleSignedIn = () => {
+
+    var domain = window.location.hostname;
+
+    $.ajax({
+        url: `${
+          domain.includes("localhost")
+            ? "http://localhost:3000"
+            : "https://api-semencescertifiees.elch.cc/"
+        }/laboratory/signedin`,
+        type: "GET",
+        xhrFields: {
+          withCredentials: true,
+        },
+        success: function (user, textStatus, xhr) {
+
+            if (xhr.status === 200) {
+                alert("You are signed in");
+            } else {
+                alert("You are not signed in");
+            }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert("You are not signed in");
+        },
+    });
+};
+
+// run the function on page load
+_handleSignedIn();
