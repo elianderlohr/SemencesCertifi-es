5fc2fec0969b6bef398d22de75618896d9d272ea
elianderlohr <elias.anderlohr@t-online.de>
2023-04-25 12:33:11 +0200

    fix: update api

--- api/app.js
+++ api/app.js
@@ -45,11 +45,15 @@ if (process.env.NODE_ENV === "production") {
 
   // CORS
   const cors = require("cors");
-  app.use(cors({ credentials: true, origin: "https://semenceescertifiees.elch.cc" }));
+
+  app.use(cors({ credentials: true, origin: /https?:\/\/(\w+\.)?elch\.cc/ }));
 
   app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", "https://semenceescertifiees.elch.cc");
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://semenceescertifiees.elch.cc"
+    );
     res.header(
       "Access-Control-Allow-Methods",
       "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
