20138c88a59300a8e3fea21078eac13a1c08807a
elianderlohr <elias.anderlohr@t-online.de>
2023-04-26 17:54:39 +0200

    fix: add post

--- api/routes/test.js
+++ api/routes/test.js
@@ -11,18 +11,33 @@ const js2xmlparser = require("js2xmlparser");
 
 // Set up a route to authenticate a user and generate a JWT token
 router.get("/info", async (req, res) => {
-    // create object
-    const info = {
-        name: "ICT4D",
-        version: "1.0.0",
-        description: "ICT4D API"}
-    
-    // return object as xml
-    res.set('Content-Type', 'text/xml');
-    res.send(js2xmlparser.parse("info", info));
+  // create object
+  const info = {
+    name: "ICT4D",
+    version: "1.0.0",
+    description: "ICT4D API",
+  };
 
+  // return object as xml
+  res.set("Content-Type", "text/xml");
+  res.send(js2xmlparser.parse("info", info));
+});
 
-    });
+// Set up a route to authenticate a user and generate a JWT token
+router.post("/post", async (req, res) => {
+  const test = req.body;
 
+  // create object
+  const info = {
+    name: "ICT4D",
+    version: "1.0.0",
+    description: "ICT4D API",
+    value: test,
+  };
+
+  // return object as xml
+  res.set("Content-Type", "text/xml");
+  res.send(js2xmlparser.parse("info", info));
+});
 
 module.exports = router;
