5f1d18ab4d2d8988f733cf0ad700f0d2ea1a2905
elianderlohr <elias.anderlohr@t-online.de>
2023-04-30 09:43:53 +0200

    fix: update

--- api/routes/test.js
+++ api/routes/test.js
@@ -13,8 +13,24 @@ const js2xmlparser = require("js2xmlparser");
 router.get("/info", async (req, res) => {
   // create object
   const info = {
+    name: "ICT4D",
+    version: "1.0.0",
+    description: "ICT4D API",
+  };
+
+  // return object as xml
+  res.set("Content-Type", "text/xml");
+  res.send(
+    js2xmlparser.parse("info", info, { declaration: { encoding: "UTF-8" } })
+  );
+});
+
+// Set up a route to authenticate a user and generate a JWT token
+router.get("/test", async (req, res) => {
+  // create object
+  const info = {
     "@": {
-      'xmlns': "https://api.semencescertifiees.elch.cc/test/namespace",
+      xmlns: "https://api.semencescertifiees.elch.cc/test/namespace",
     },
     info: {
       name: "ICT4D",
@@ -30,11 +46,12 @@ router.get("/info", async (req, res) => {
   );
 });
 
-router.get("/namespace"), async (req, res) => {
-  // create object
-  res.set("Content-Type", "text/xml");
-  res.send(
-    `<?xml version="1.0" encoding="utf-8"?>
+router.get("/namespace"),
+  async (req, res) => {
+    // create object
+    res.set("Content-Type", "text/xml");
+    res.send(
+      `<?xml version="1.0" encoding="utf-8"?>
     <!-- Created with Liquid Technologies Online Tools 1.0 (https://www.liquid-technologies.com) -->
     <xs:schema xmlns:ns="some_identifier" attributeFormDefault="unqualified" elementFormDefault="qualified" xmlns:xs="http://www.w3.org/2001/XMLSchema">
       <xs:element name="info">
@@ -53,8 +70,8 @@ router.get("/namespace"), async (req, res) => {
         </xs:complexType>
       </xs:element>
     </xs:schema>`
-  );
-};
+    );
+  };
 
 // Set up a route to authenticate a user and generate a JWT token
 router.post("/post", async (req, res) => {
