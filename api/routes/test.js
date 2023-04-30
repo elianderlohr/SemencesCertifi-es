7a6cf9f4add5265c8c32235cffcf7da5ad0619db
elianderlohr <elias.anderlohr@t-online.de>
2023-04-30 09:40:31 +0200

    fix: add custom namespace

--- api/routes/test.js
+++ api/routes/test.js
@@ -14,7 +14,7 @@ router.get("/info", async (req, res) => {
   // create object
   const info = {
     "@": {
-      'xmlns:ns': "some_identifier",
+      'xmlns': "https://api.semencescertifiees.elch.cc/test/namespace",
     },
     info: {
       name: "ICT4D",
@@ -29,6 +29,32 @@ router.get("/info", async (req, res) => {
     js2xmlparser.parse("info", info, { declaration: { encoding: "UTF-8" } })
   );
 });
+
+router.get("/namespace"), async (req, res) => {
+  // create object
+  res.set("Content-Type", "text/xml");
+  res.send(
+    `<?xml version="1.0" encoding="utf-8"?>
+    <!-- Created with Liquid Technologies Online Tools 1.0 (https://www.liquid-technologies.com) -->
+    <xs:schema xmlns:ns="some_identifier" attributeFormDefault="unqualified" elementFormDefault="qualified" xmlns:xs="http://www.w3.org/2001/XMLSchema">
+      <xs:element name="info">
+        <xs:complexType>
+          <xs:sequence>
+            <xs:element name="info">
+              <xs:complexType>
+                <xs:sequence>
+                  <xs:element name="name" type="xs:string" />
+                  <xs:element name="version" type="xs:string" />
+                  <xs:element name="description" type="xs:string" />
+                </xs:sequence>
+              </xs:complexType>
+            </xs:element>
+          </xs:sequence>
+        </xs:complexType>
+      </xs:element>
+    </xs:schema>`
+  );
+};
 
 // Set up a route to authenticate a user and generate a JWT token
 router.post("/post", async (req, res) => {
