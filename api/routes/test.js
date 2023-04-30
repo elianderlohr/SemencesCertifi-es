11c8ee1b167465c6dd9225eaa333bf2fd4a36985
elianderlohr <elias.anderlohr@t-online.de>
2023-04-30 09:26:53 +0200

    fix: another namespace

--- api/routes/test.js
+++ api/routes/test.js
@@ -14,7 +14,7 @@ router.get("/info", async (req, res) => {
   // create object
   const info = {
     "@": {
-      xmlns: "http://www.w3.org/2001/vxml",
+      xmlns: "http://www.w3.org/TR/html4/",
     },
     info: {
       name: "ICT4D",
